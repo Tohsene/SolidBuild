ca4fcb34ac2a39a63f801a053d40b78297386b28
support <support@DESKTOP-GOA92KN>
2021-11-12 11:29:38 +0100

    Add project files.

---
+++ SolidBuild/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace SolidBuild
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //leads by example
+            //takes care of parent
+            //takes care of younger ones
+            //commands and settles disputes
+
+            //public void FirstChildResp() { };
+            
+        }
+
+        public class FirstChildRole
+        {
+            public OurLeader ourLeader;
+            public IOurAltruistic ourAltruistic;
+            public OurRoleModel ourRoleModel;
+
+            public FirstChildRole(OurLeader ourLeader, IOurAltruistic ourAltruistic, OurRoleModel ourRoleModel)
+            {
+                this.ourLeader = ourLeader;
+                this.ourAltruistic = ourAltruistic;
+                this.ourRoleModel = ourRoleModel;
+            }
+            
+            public void functions()
+            {
+                ourLeader.Leader();
+                ourAltruistic.Altruistic();
+                ourRoleModel.RoleModel();
+            }
+        }
+        public class OurLeader
+        {
+            public void Leader() { }
+        }
+
+        public interface IOurAltruistic
+        {
+            void Altruistic();
+               
+        }
+        public interface IConfident
+        {
+            void Confident();
+        }
+        public interface IProactive
+        {
+            void Proactive();
+        }
+
+        public class AltruisticToParents : IOurAltruistic
+        {
+            public void Altruistic() { }
+        }
+        public class AltruisticToOthers : IOurAltruistic
+        {
+            public void Altruistic() 
+            {
+                throw new NotImplementedException();
+            }
+        }
+        public class OurRoleModel
+        {
+            public void RoleModel() { }
+        }
+
+    }
+}
---
+++ SolidBuild/SoftwareEngineering.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidBuild
+{
+    //liskov principle
+    public class SoftwareEngineering
+    {
+    }
+
+    public class BackEnd : SoftwareEngineering
+    {
+        public void learnOop() { }
+    }
+
+    public class FrontEnd : SoftwareEngineering { }
+
+    public class CSharp : BackEnd { }
+
+}
